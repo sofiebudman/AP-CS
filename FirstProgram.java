4138e623d88e3f6064f3807f5adc67b825dd5150
Sofie Budman <105175854+sofiebudman@users.noreply.github.com>
2024-08-20 13:32:01 -0700

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class FirstProgram here.
+ *
+ * @author Sofie Budman
+ * @version 8/20/2024
+ * description: This is my first program
+ */
+public class FirstProgram
+{
+    public static void main(String[] args){
+        // creates the 5 variables
+        int num = 99;
+        double price = 7.99;
+        String fruit = "peach";
+        boolean flag = true;
+        final double GRAVITY = 9.81;
+        
+        //prints out the variables
+        System.out.println("The number is "+ num + ".\nThe price is $" + price + ".\nThe fruit is " + fruit + ".\nThe flag is " + flag + ".\nGravity is " + GRAVITY);
+    }
+    // instance variables - replace the example below with your own
+ 
+}
