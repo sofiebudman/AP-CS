656b37a1d4373e937dd7b3b0c57948b490f8964f
Sofie Budman <105175854+sofiebudman@users.noreply.github.com>
2024-08-22 13:57:28 -0700

    Update ScannerTest.java

--- ScannerTest.java
+++ ScannerTest.java
@@ -11,14 +11,16 @@ public class ScannerTest
         Scanner sc = new Scanner(System.in);
         System.out.println("What grade are you in?");
         int grade = sc.nextInt();
-        System.out.println("You are in the " + grade + "th grade");
+        System.out.println("You are in the " + grade + "th grade \n");
+        
         System.out.print("What is your name? ");
         sc.nextLine();
         String name = sc.nextLine();
-        System.out.println("Welcome, " + name);
+        System.out.println("Welcome, " + name + "\n");
         System.out.println("How much do you get in allowance per week?");
         double allowance = sc.nextDouble();
         System.out.println("Wow! " + allowance + " is a lot of money"); 
+        sc.close();
     }
     // instance variables - replace the example below with your own
    
